6aa72b4ba39034768a9e72d7e8ef0f96695854f1
Consultant <samirsamado5@gmail.com>
2024-02-27 20:38:36 -0500

    custom error pages

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def index():
 def user():
     list_fruits = [1, 1, 2]
     return render_template("user.html", list_fruits=list_fruits)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'),  404
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'),  500
+
