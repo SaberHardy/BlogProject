85188b61d5bb2aa0e21591d2cfc3a94581ab36ae
Consultant <samirsamado5@gmail.com>
2024-02-27 21:27:03 -0500

    add base and navbar

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+@app.route('/')
 def index():
     return render_template("index.html")
 
@@ -15,9 +16,9 @@ def user():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'),  404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def page_not_found(error):
-    return render_template('500.html'),  500
-
+    return render_template('500.html'), 500
