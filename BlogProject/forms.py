3e60dcf31915a0449c02dea754c90ce7ca2e8602
SaberHardy <samirsamado5@gmail.com>
2024-03-10 22:07:21 -0400

    add user and render all users

---
+++ BlogProject/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
--- BlogProject/main.py
+++ BlogProject/main.py
@@ -5,6 +5,8 @@ from wtforms.validators import DataRequired
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
+from forms import UserForm
+
 app = Flask(__name__, template_folder='templates')  # static_folder='static')
 
 # Create a Secret Key
@@ -28,6 +30,30 @@ class UsersModel(db.Model):
 class NameForm(FlaskForm):
     name = StringField("What's your name", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+@app.route('/user/add', methods=["Post", "Get"])
+def add_user():
+    name = None
+    form = UserForm()
+
+    if form.validate_on_submit():
+        user = UsersModel.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = UsersModel(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User added")
+
+    all_users = UsersModel.query.order_by(UsersModel.date_added)
+    return render_template("add_user.html",
+                           form=form,
+                           name=name,
+                           all_users=all_users)
 
 
 @app.route('/')
