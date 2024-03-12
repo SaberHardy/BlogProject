84dbd9cd2e40360f9fc95a1731f7fc0eb23745b6
SaberHardy <samirsamado5@gmail.com>
2024-03-11 22:18:02 -0400

    update user

--- BlogProject/main.py
+++ BlogProject/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from models import UsersModel, db
 from forms import UserForm
+from secret_staff import SECRET_STAFF
 
 app = Flask(__name__, template_folder='templates')  # static_folder='static')
 
@@ -10,7 +11,7 @@ app.config["SECRET_KEY"] = "mySecretKey"
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 # My SqlDB
 # app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://username:password@localhost/database_name"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:PASSWORD@localhost/our_users"
+app.config["SQLALCHEMY_DATABASE_URI"] = SECRET_STAFF
 
 db.init_app(app)
 
@@ -70,3 +71,28 @@ def name():
         flash("The form submitted successfully!", "info")
 
     return render_template('name.html', name=name, form=form)
+
+
+@app.route('/update/<int:id>/', methods=['POST', 'GET'])
+def update_user(id):
+    form = UserForm(request.form)
+    user_to_update = UsersModel.query.get_or_404(id)
+    if request.method == 'POST':
+        user_to_update.name = request.form['name']
+        user_to_update.email = request.form['email']
+
+        try:
+            db.session.commit()
+            flash("User updated successfully!")
+            return render_template("update_user.html",
+                                   form=form,
+                                   user_to_update=user_to_update)
+        except:
+            flash("Error user")
+            return render_template("update_user.html",
+                                   form=form,
+                                   user_to_update=user_to_update)
+    else:
+        return render_template("update_user.html",
+                               form=form,
+                               user_to_update=user_to_update)
