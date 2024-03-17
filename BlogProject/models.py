c195d89cbc73265ae27a3e3fe925a02746f21b00
SaberHardy <samirsamado5@gmail.com>
2024-03-17 15:49:10 -0400

    create post model

--- BlogProject/models.py
+++ BlogProject/models.py
@@ -2,8 +2,16 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 db = SQLAlchemy()
+
+
+class PostModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text())
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    slug = db.Column(db.String(255))
 
 
 class UsersModel(db.Model):
